ef31d49fc8b102b0802ed1747834f63dbcd97994
Alessandro Chinchilla <ale030401@gmail.com>
2024-02-13 12:52:43 -0600

    Created function to process API data

--- src/modules/logic.js
+++ src/modules/logic.js
@@ -1,21 +1,35 @@
 const ApiKey = "59d1ace8b6504eab8cf182808240702";
-const main = document.querySelector("main");
-
+const img = document.querySelector("img");
+const p = document.getElementById("name");
+/*
 function showData(data) {
-  const p = document.createElement("p");
-
-  p.innerHTML = data;
-  main.appendChild(p);
+  p.innerHTML = data.name;
+  img.src = data.condition.icon;
 }
+*/
 function processData(data) {
-  const celciusData = data.current.temp_c;
-  const country = data.current.location;
-  showData(celciusData);
-  return { celciusData, country };
+  const dataArray = {
+    celciusData: data.current.temp_c,
+    fahrenheitData: data.current.temp_f,
+    feelsLikeC: data.current.feelslike_c,
+    feelsLikeF: data.current.feelslike_f,
+    humidity: data.current.humidity,
+    windKph: data.current.wind_kph,
+    windMph: data.current.wind_mph,
+    name: data.location.name,
+    region: data.location.region,
+    condition: data.current.condition,
+    uv: data.current.uv,
+    precipMm: data.current.precip_mm,
+    precipInch: data.current.precip_in,
+  };
+  console.log(dataArray);
+  // showData(dataArray);
+  return { dataArray };
 }
 
 async function getData(location) {
-  console.log(location);
+  // console.log(location);
 
   if (location === "") {
     console.log("Please enter a location ");
@@ -24,21 +38,15 @@ async function getData(location) {
 
   try {
     const response = await fetch(
-      `https://api.weatherapi.com/v1/current.json?key=${ApiKey}&q=${location}`,
+      `https://api.weatherapi.com/v1/current.json?key=${ApiKey}&q=${location}&days=3`,
       { mode: "cors" },
     );
     const weatherData = await response.json();
     console.log(weatherData);
     processData(weatherData);
-    // showData();
   } catch (err) {
     console.log(err);
   }
 }
 
 export { getData };
-
-/*
-  const celciusData = weatherData.current.temp_c;
-  const { country } = weatherData.location;
-*/
