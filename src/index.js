116a1c70b88d9489593d586539a7ecdb6fe7ce57
Alessandro Chinchilla <ale030401@gmail.com>
2024-02-20 12:55:16 -0600

    Add: logic for showinf Fahrenheit data & separated dom logic

--- src/index.js
+++ src/index.js
@@ -2,9 +2,21 @@ import { getData, getForecast } from "./modules/logic";
 
 const searchButton = document.getElementById("search-button");
 const input = document.getElementById("location-search");
+const fahrenBuutton = document.getElementById("fahren-button");
+const celciusButton = document.getElementById("celcius-button");
 
 searchButton.addEventListener("click", (e) => {
   e.preventDefault();
   getData(input.value);
   getForecast(input.value);
+});
+
+fahrenBuutton.addEventListener("click", () => {
+  getData(input.value, true);
+  getForecast(input.value, true);
+});
+
+celciusButton.addEventListener("click", () => {
+  getData(input.value, false);
+  getForecast(input.value, false);
 });
--- src/modules/logic.js
+++ src/modules/logic.js
@@ -1,76 +1,19 @@
+/* eslint-disable camelcase */
+import {
+  showDataCurrent_C,
+  showDataCurrent_F,
+  showDataForecast_C,
+  showDataForecast_F,
+} from "./dom";
+
 const ApiKey = "59d1ace8b6504eab8cf182808240702";
-// DOM elements for current day
-const locationName = document.getElementById("location-name");
-const condition = document.getElementById("condition-0");
-const temp = document.getElementById("temp");
-const feelsLike = document.getElementById("feels-like");
-const humidity = document.getElementById("humidity");
-const wind = document.getElementById("wind");
-const precipitation = document.getElementById("precipitation");
-const uvIndex = document.getElementById("uv-index");
-const minTemp0 = document.getElementById("min-temp-0");
-const maxTemp0 = document.getElementById("max-temp-0");
-// DOM elements for forecast
-const condition1 = document.getElementById("condition-1");
-const minTemp1 = document.getElementById("min-temp-1");
-const maxTemp1 = document.getElementById("max-temp-1");
-const condition2 = document.getElementById("condition-2");
-const minTemp2 = document.getElementById("min-temp-2");
-const maxTemp2 = document.getElementById("max-temp-2");
-const condition3 = document.getElementById("condition-3");
-const minTemp3 = document.getElementById("min-temp-3");
-const maxTemp3 = document.getElementById("max-temp-3");
 
-function showDataCurrent(data) {
-  locationName.innerHTML = `${data.name}, ${data.country}`;
-  condition.innerHTML = data.condition;
-  temp.innerHTML = data.celciusData;
-  feelsLike.innerHTML = data.feelsLikeC;
-  humidity.innerHTML = data.humidity;
-  wind.innerHTML = data.windKph;
-  precipitation.innerHTML = data.precipMm;
-  uvIndex.innerHTML = data.uv;
-}
-
-function showDataForecast(data) {
-  minTemp0.innerHTML = data.mintemp_c_0;
-  maxTemp0.innerHTML = data.maxtemp_c_0;
-  condition1.innerHTML = data.condition_1;
-  minTemp1.innerHTML = data.mintemp_c_1;
-  maxTemp1.innerHTML = data.maxtemp_c_1;
-  condition2.innerHTML = data.condition_2;
-  minTemp2.innerHTML = data.mintemp_c_2;
-  maxTemp2.innerHTML = data.maxtemp_c_2;
-  condition3.innerHTML = data.condition_3;
-  minTemp3.innerHTML = data.mintemp_c_3;
-  maxTemp3.innerHTML = data.maxtemp_c_3;
-}
-
-function showDataFahren(data) {
-  // temp.innerHTML = data.fahrenheitData;
-  // feelsLike.innerHTML = data.feelsLikeF;
-  minTemp0.innerHTML = data.mintemp_f_0;
-  maxTemp0.innerHTML = data.maxtemp_f_0;
-  condition1.innerHTML = data.condition_1;
-  minTemp1.innerHTML = data.mintemp_f_1;
-  maxTemp1.innerHTML = data.maxtemp_f_1;
-  condition2.innerHTML = data.condition_2;
-  minTemp2.innerHTML = data.mintemp_f_2;
-  maxTemp2.innerHTML = data.maxtemp_f_2;
-  condition3.innerHTML = data.condition_3;
-  minTemp3.innerHTML = data.mintemp_f_3;
-  maxTemp3.innerHTML = data.maxtemp_f_3;
-}
-
-function processData(data) {
-  const dataArray = {
+function processData(data, displayF) {
+  const dataObject_C = {
     celciusData: data.current.temp_c,
-    fahrenheitData: data.current.temp_f,
     feelsLikeC: data.current.feelslike_c,
-    feelsLikeF: data.current.feelslike_f,
     humidity: data.current.humidity,
     windKph: data.current.wind_kph,
-    windMph: data.current.wind_mph,
     name: data.location.name,
     country: data.location.country,
     condition: data.current.condition.text,
@@ -78,43 +21,68 @@ function processData(data) {
     precipMm: data.current.precip_mm,
     precipInch: data.current.precip_in,
   };
-  console.log(dataArray);
-  showDataCurrent(dataArray);
-  return { dataArray };
+
+  const dataObject_F = {
+    fahrenheitData: data.current.temp_f,
+    feelsLikeF: data.current.feelslike_f,
+    humidity: data.current.humidity,
+    windKph: data.current.wind_kph,
+    name: data.location.name,
+    country: data.location.country,
+    condition: data.current.condition.text,
+    uv: data.current.uv,
+    precipMm: data.current.precip_mm,
+    precipInch: data.current.precip_in,
+  };
+
+  if (displayF) {
+    showDataCurrent_F(dataObject_F);
+  } else {
+    showDataCurrent_C(dataObject_C);
+  }
 }
 
-function processForecastData(data) {
-  const forecastDataArray = {
+function processForecastData(data, displayF) {
+  const forecastDataObject_C = {
     maxtemp_c_0: data.forecast.forecastday[0].day.maxtemp_c,
     mintemp_c_0: data.forecast.forecastday[0].day.mintemp_c,
-    maxtemp_f_0: data.forecast.forecastday[0].day.maxtemp_f,
-    mintemp_f_0: data.forecast.forecastday[0].day.mintemp_f,
     condition_0: data.forecast.forecastday[0].day.condition.text,
     maxtemp_c_1: data.forecast.forecastday[1].day.maxtemp_c,
     mintemp_c_1: data.forecast.forecastday[1].day.mintemp_c,
-    maxtemp_f_1: data.forecast.forecastday[1].day.maxtemp_f,
-    mintemp_f_1: data.forecast.forecastday[1].day.mintemp_f,
     condition_1: data.forecast.forecastday[1].day.condition.text,
     maxtemp_c_2: data.forecast.forecastday[2].day.maxtemp_c,
     mintemp_c_2: data.forecast.forecastday[2].day.mintemp_c,
-    maxtemp_f_2: data.forecast.forecastday[2].day.maxtemp_f,
-    mintemp_f_2: data.forecast.forecastday[2].day.mintemp_f,
     condition_2: data.forecast.forecastday[2].day.condition.text,
     maxtemp_c_3: data.forecast.forecastday[3].day.maxtemp_c,
     mintemp_c_3: data.forecast.forecastday[3].day.mintemp_c,
-    maxtemp_f_3: data.forecast.forecastday[3].day.maxtemp_f,
-    mintemp_f_3: data.forecast.forecastday[3].day.mintemp_f,
     condition_3: data.forecast.forecastday[3].day.condition.text,
   };
-  console.log(forecastDataArray);
-  showDataForecast(forecastDataArray);
+
+  const forecastDataObject_F = {
+    maxtemp_f_0: data.forecast.forecastday[0].day.maxtemp_f,
+    mintemp_f_0: data.forecast.forecastday[0].day.mintemp_f,
+    maxtemp_f_1: data.forecast.forecastday[1].day.maxtemp_f,
+    mintemp_f_1: data.forecast.forecastday[1].day.mintemp_f,
+    maxtemp_f_2: data.forecast.forecastday[2].day.maxtemp_f,
+    mintemp_f_2: data.forecast.forecastday[2].day.mintemp_f,
+    maxtemp_f_3: data.forecast.forecastday[3].day.maxtemp_f,
+    mintemp_f_3: data.forecast.forecastday[3].day.mintemp_f,
+    condition_0: data.forecast.forecastday[0].day.condition.text,
+    condition_1: data.forecast.forecastday[1].day.condition.text,
+    condition_2: data.forecast.forecastday[2].day.condition.text,
+    condition_3: data.forecast.forecastday[3].day.condition.text,
+  };
+
+  if (displayF) {
+    showDataForecast_F(forecastDataObject_F);
+  } else {
+    showDataForecast_C(forecastDataObject_C);
+  }
 }
 
-async function getData(location) {
-  // console.log(location);
-
+async function getData(location, displayF = false) {
   if (location === "") {
-    console.log("Please enter a location ");
+    console.log("Please enter a location "); // Change this!!
     return;
   }
 
@@ -124,14 +92,14 @@ async function getData(location) {
       { mode: "cors" },
     );
     const weatherData = await response.json();
-    console.log(weatherData);
-    processData(weatherData);
+    // console.log(weatherData);
+    processData(weatherData, displayF);
   } catch (err) {
-    console.log(err);
+    console.log(err); // Change Error Handling
   }
 }
 
-async function getForecast(location) {
+async function getForecast(location, displayF = false) {
   if (location === "") {
     return;
   }
@@ -142,11 +110,11 @@ async function getForecast(location) {
       { mode: "cors" },
     );
     const forecastData = await response.json();
-    console.log(forecastData);
-    processForecastData(forecastData);
+    // console.log(forecastData);
+    processForecastData(forecastData, displayF);
   } catch (err) {
-    console.log(err);
+    console.log(err); // Change Error Handling
   }
 }
 
-export { getData, getForecast, showDataCurrent };
+export { getData, getForecast };
